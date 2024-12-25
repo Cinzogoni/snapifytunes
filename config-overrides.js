95fd420a9e282e28d7a1463613b3716c1b0a453d
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2024-12-25 07:31:46 +0700

    snapifytunes_Web-v1.0.0_FIX-16

--- config-overrides.js
+++ config-overrides.js
@@ -1,27 +1,3 @@
 const { override, useBabelRc } = require("customize-cra");
-const path = require("path");
 
-module.exports = override(
-  useBabelRc(),
-
-  (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      svgo: "svgo@latest",
-      "nth-check": "nth-check@latest",
-      postcss: "postcss@latest",
-      "resolve-url-loader": "resolve-url-loader@latest",
-      "~": path.resolve(__dirname, "src"),
-    };
-
-    const plugins = config.plugins.map((plugin) => {
-      if (plugin.constructor.name === "MiniCssExtractPlugin") {
-        plugin.options.ignoreOrder = true;
-      }
-      return plugin;
-    });
-    config.plugins = plugins;
-
-    return config;
-  }
-);
+module.exports = override(useBabelRc());
